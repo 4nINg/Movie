0921d47c334fe60e522302e9c5cd93163d6a66b2
4nIng <42165498+anjunggeun@users.noreply.github.com>
2019-05-31 22:59:01 +0900

    MainNavigation

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -1,10 +1,10 @@
-import TabNavigation from "./TabNavigation"
-import DetailScreen from "../screens/Detail"
-import { createStackNavigator, createAppContainer } from "react-navigation"
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import TabNavigation from "./TabNavigation";
+import DetailScreen from "../screens/Detail";
 
 const MainNavigation = createStackNavigator({
-  TabNavigation,
-  DetailScreen
-})
+  Tab: { screen: TabNavigation, navigationOptions: { header: null } },
+  Detail: DetailScreen
+});
 
-export default createAppContainer(MainNavigation)
+export default createAppContainer(MainNavigation);
